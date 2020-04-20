15f5b645487d4b7982d95f3efde2253f790c29dd
Steph W <wilhelm.steph@gmail.com>
2020-04-20 20:23:20 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -28,8 +28,8 @@ def fourth_challenge
 		        :attitude => "confused"
 	      },
 	      {
-		        :name => "Nurse", 
-		        :age => "40",
+		        :name => "Nurse",
+		        :age => "44",
 		        :attitude => "worried"
 	      }
       ]
